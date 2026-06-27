ccffb20b7aedb2687a2fd1f898e3ffd0cb6d217f
Bernard Barrois <33697798+Barrois@users.noreply.github.com>
2026-06-27 10:31:48 +0200

    deploy: mise a jour VigieNid

--- deploy_nidtraque.ps1
+++ deploy_nidtraque.ps1
@@ -1,18 +1,9 @@
 # deploy_nidtraque.ps1
+# Dépose les fichiers directement dans le dossier NidTraque puis pushe
 $DEST = "C:\Users\berna\Documents\Arduino\Michel_Lehmann\Pot_a_meche\Application_Smartphone\NidTraque"
 
-if (Test-Path "$DEST\index_new.html") {
-  Move-Item "$DEST\index_new.html" "$DEST\index.html" -Force
-  Write-Host "index_new.html -> index.html" -ForegroundColor Cyan
-} elseif (Test-Path "$DEST\index.html") {
-  Write-Host "index.html deja en place" -ForegroundColor Yellow
-} else {
-  Write-Host "Aucun fichier index trouve" -ForegroundColor Red
-  exit
-}
-
 cd $DEST
-git add index.html
+git add .
 git commit -m "deploy: mise a jour VigieNid"
 git push
 Write-Host "Deploy termine !" -ForegroundColor Green
